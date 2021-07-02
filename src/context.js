ffea12bd7fa692e7fb3405656c15e0f286d03a40
Zack DB DEV <zackinfosbd@gmail.com>
2021-07-02 22:09:28 +0100

    final contextAPI boiletplate

--- src/context.js
+++ src/context.js
@@ -3,7 +3,7 @@ import sublinks from "./data";
 
 const AppContext = React.createContext();
 
-const AppProvider = ({ children }) => {
+export const AppProvider = ({ children }) => {
   const { isSidebarOpen, setIsSidebarOpen } = useState(true);
   const { isSubmenuOpen, setIsSubmenuOpen } = useState(true);
 
@@ -33,4 +33,8 @@ const AppProvider = ({ children }) => {
       {children}
     </AppContext.Provider>
   );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
 };
